11a5d3dc94d2f1e5bcbb8e47ea338e12d2fb7aea
tao-xinxiu <isabelle.tao@orange.com>
2018-05-20 22:44:25 +0200

    refactoring: remove duplicate transitions in Mix strategy, make it extend from Canary strategy

--- src/main/java/com/orange/strategy/impl/BlueGreenCanaryMixStrategy.java
+++ src/main/java/com/orange/strategy/impl/BlueGreenCanaryMixStrategy.java
@@ -9,17 +9,16 @@ import org.slf4j.LoggerFactory;
 import com.orange.model.StrategyConfig;
 import com.orange.model.architecture.Architecture;
 import com.orange.model.architecture.Microservice;
-import com.orange.strategy.TagUpdatingVersionStrategy;
 import com.orange.strategy.Transition;
 import com.orange.util.SetUtil;
 
-public class BlueGreenCanaryMixStrategy extends TagUpdatingVersionStrategy {
+public class BlueGreenCanaryMixStrategy extends CanaryStrategy {
     private static final Logger logger = LoggerFactory.getLogger(BlueGreenCanaryMixStrategy.class);
 
     public BlueGreenCanaryMixStrategy(StrategyConfig config) {
 	super(config);
-	transitions = Arrays.asList(addCanaryTransit, updateExceptInstancesRoutesTransit, library.updateRouteBeforeNbProcTransit,
-		rolloutTransit, library.removeUndesiredTransit);
+	transitions = Arrays.asList(addCanaryTransit, updateExceptInstancesRoutesTransit,
+		library.updateRouteBeforeNbProcTransit, scaleTransit, library.removeUndesiredTransit);
     }
 
     @Override
@@ -28,99 +27,28 @@ public class BlueGreenCanaryMixStrategy extends TagUpdatingVersionStrategy {
     }
 
     /**
-     * next architecture: add canary microservice with new pkg and env
+     * next architecture: scale up desired microservice and rollout old ones
      */
-    protected Transition addCanaryTransit = new Transition() {
-	@Override
-	public Architecture next(Architecture currentArchitecture, Architecture finalArchitecture) {
-	    Architecture nextArchitecture = new Architecture(currentArchitecture);
-	    for (String site : finalArchitecture.listSitesName()) {
-		Set<Microservice> currentMicroservices = nextArchitecture.getSiteMicroservices(site);
-		for (Microservice desiredMicroservice : finalArchitecture.getSiteMicroservices(site)) {
-		    if (SetUtil.noneMatch(currentMicroservices,
-			    ms -> ms.eqAttr(Arrays.asList("name", "path", "env"), desiredMicroservice))) {
-			Microservice updatingMs = SetUtil.getUniqueMicroservice(currentMicroservices,
-				(String) desiredMicroservice.get("name"), config.getUpdatingVersion());
-			if (updatingMs != null) {
-			    // copy all properties except id, version
-			    Microservice newMicroservice = new Microservice(desiredMicroservice);
-			    newMicroservice.copyAttr(Arrays.asList("guid", "version"), updatingMs);
-			    updatingMs = newMicroservice;
-			} else {
-			    Microservice newMicroservice = new Microservice(desiredMicroservice);
-			    newMicroservice.set("guid", null);
-			    if (newMicroservice.get("version") == null) {
-				newMicroservice.set("version", config.getUpdatingVersion());
-			    }
-			    newMicroservice.set("routes", library.tmpRoute(site, desiredMicroservice));
-			    newMicroservice.set("nbProcesses", config.getCanaryNbr());
-			    nextArchitecture.getSite(site).addMicroservice(newMicroservice);
-			    logger.info("Added a new microservice: {} ", newMicroservice);
-			}
-			continue;
-		    }
-		}
-	    }
-	    return nextArchitecture;
-	}
-    };
-
-    /**
-     * next architecture: update desired microservice properties except nbrProcesses
-     * and routes
-     */
-    protected Transition updateExceptInstancesRoutesTransit = new Transition() {
-	// assume that it doesn't exist two microservices with same pkg and name
+    protected Transition scaleTransit = new Transition() {
 	@Override
 	public Architecture next(Architecture currentArchitecture, Architecture finalArchitecture) {
 	    Architecture nextArchitecture = new Architecture(currentArchitecture);
 	    for (String site : finalArchitecture.listSitesName()) {
 		for (Microservice desiredMs : finalArchitecture.getSiteMicroservices(site)) {
-		    if (SetUtil.noneMatch(nextArchitecture.getSiteMicroservices(site),
-			    ms -> ms.eqAttr(Arrays.asList("name", "path", "env", "services", "state"), desiredMs)
-				    && ms.get("version").equals(config.getUpdatingVersion()))) {
-			Microservice nextMicroservice = SetUtil.getUniqueMicroservice(
-				nextArchitecture.getSiteMicroservices(site), ms -> ms.eqAttr("name", desiredMs)
-					&& ms.get("version").equals(config.getUpdatingVersion()));
-			nextMicroservice.copyAttr(Arrays.asList("path", "env", "services", "state"), desiredMs);
-			nextMicroservice.set("routes", library.tmpRoute(site, desiredMs));
-			logger.info("Updated microservice [{}_{}] to {} ", nextMicroservice.get("name"),
-				nextMicroservice.get("version"), nextMicroservice);
-		    }
-		}
-	    }
-	    return nextArchitecture;
-	}
-    };
-
-    
-
-    /**
-     * next architecture: scale up desired microservice and rollout old ones
-     */
-    protected Transition rolloutTransit = new Transition() {
-	@Override
-	public Architecture next(Architecture currentArchitecture, Architecture finalArchitecture) {
-	    Architecture nextArchitecture = new Architecture(currentArchitecture);
-	    for (String site : finalArchitecture.listSitesName()) {
-		for (Microservice desiredMicroservice : finalArchitecture.getSiteMicroservices(site)) {
-		    Set<Microservice> nextMicroservices = nextArchitecture.getSiteMicroservices(site);
-		    if (SetUtil.noneMatch(nextMicroservices, ms -> ms.isInstantiation(desiredMicroservice))) {
-			for (Microservice nextMicroservice : SetUtil.searchByName(nextMicroservices,
-				(String) desiredMicroservice.get("name"))) {
-			    if (nextMicroservice.get("version").equals(library.desiredVersion(desiredMicroservice))) {
-				int nextNbr = (int) nextMicroservice.get("nbProcesses") + config.getCanaryIncrease();
-				if (nextNbr > (int) desiredMicroservice.get("nbProcesses")) {
-				    nextNbr = (int) desiredMicroservice.get("nbProcesses");
-				}
-				nextMicroservice.set("nbProcesses", nextNbr);
+		    Set<Microservice> nextMss = nextArchitecture.getSiteMicroservices(site);
+		    if (SetUtil.noneMatch(nextMss, ms -> ms.isInstantiation(desiredMs))) {
+			for (Microservice nextMs : SetUtil.searchByName(nextMss, (String) desiredMs.get("name"))) {
+			    int desiredNbProc = (int) desiredMs.get("nbProcesses");
+			    if (nextMs.get("version").equals(library.desiredVersion(desiredMs))) {
+				int nextNbr = (int) nextMs.get("nbProcesses") + config.getCanaryIncrease();
+				nextNbr = nextNbr > desiredNbProc ? desiredNbProc : nextNbr;
+				nextMs.set("nbProcesses", nextNbr);
 			    } else {
-				int nextNbr = (int) nextMicroservice.get("nbProcesses") - config.getCanaryIncrease();
-				if (nextNbr < 1) {
-				    nextNbr = 1;
-				}
-				nextMicroservice.set("nbProcesses", nextNbr);
+				int nextNbr = (int) nextMs.get("nbProcesses") - config.getCanaryIncrease();
+				nextNbr = nextNbr < 1 ? 1 : nextNbr;
+				nextMs.set("nbProcesses", nextNbr);
 			    }
+			    logger.info("scaled microservice {}", nextMs);
 			}
 		    }
 		}
