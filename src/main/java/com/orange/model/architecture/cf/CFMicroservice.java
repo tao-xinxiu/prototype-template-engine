ebcebca7469c7b8730ccb28b7fc8d3cd06c9b6ab
tao-xinxiu <isabelle.tao@orange.com>
2018-09-07 18:09:54 +0200

    fix: services or routes null

--- src/main/java/com/orange/model/architecture/cf/CFMicroservice.java
+++ src/main/java/com/orange/model/architecture/cf/CFMicroservice.java
@@ -1,5 +1,6 @@
 package com.orange.model.architecture.cf;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -12,7 +13,13 @@ public class CFMicroservice extends Microservice {
 	super(microservice);
 	set("name", microservice.get("name") + "_" + microservice.get("version"));
 	set("state", CFMicroserviceState.valueOf(microservice.get("state").toString()));
+	if (microservice.get("routes") == null) {
+	    set("routes", new HashSet<>());
+	}
 	set("routes", ((Set<String>) microservice.get("routes")).stream().map(Route::new).collect(Collectors.toSet()));
+	if (microservice.get("services") == null) {
+	    set("services", new HashSet<>());
+	}
     }
 
     public CFMicroservice(Map<String, Object> attributes) {
